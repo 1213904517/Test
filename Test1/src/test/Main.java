57b93b3e2340055c311b369d334d8e2edbda2852
1213904517 <1213904517@qq.com>
2017-10-18 21:50:55 +0800

    Java Exercise Code

---
+++ Test1/src/test/Main.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Main {
+
+	public static void main(String []args){
+		Test a=new Test();
+		a.NarcissisticNumber();
+		a.YearType();
+		a.PrintGraph();
+		a.ToGradeScore();
+			}
+
+}
